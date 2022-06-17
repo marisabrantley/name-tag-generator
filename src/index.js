7cb61c8640b35c843b41b6c3bc4aebbce61ffc3c
Marisa Brantley <60168324+marisabrantley@users.noreply.github.com>
2022-06-16 19:59:23 -0700

    Update index.js with callback function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import NameTag from "./NameTag.js";
+import { names } from "./data.js";
 
 import "./styles.css";
 
-const App = () => (
-  <div className="App">
-    <h1>Name Tag Generator</h1>
-    <NameTag name="Mike" />
-    <NameTag name="Lily" />
-    <NameTag name="Sophie" />
-    <NameTag name="Sadie" />
-  </div>
-);
+const renderNameTag = (name) => <NameTag name={name.person} key={name.id} />;
+
+const App = () => {
+  const App = () => {
+  const NameTagElements = names.map(renderNameTag);
+    
+  return (  
+     <div className="App">
+      <h1>Name Tag Generator</h1>
+      {NameTagElements}
+    </div>
+  );
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
